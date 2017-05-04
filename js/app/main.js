0ff7ffc882ff32216b45bceea107f3030261ef67
Travis Clarke <travis.m.clarke@gmail.com>
2017-05-03 23:39:00 -0700

    update to use TableExport v4.0.0-rc.4

--- js/app/main.js
+++ js/app/main.js
@@ -3,11 +3,8 @@ define(["jquery", "tableexport"], function ($, TableExport) {
     // DOM ready
     $(function () {
 
-        // Initialize TableExport using jQuery.
-        $('#table1').tableExport();
-
         // Initialize TableExport using vanilla JS.
-        var $selector = $("#table2");
+        var $selector = document.getElementById("table2");
         new TableExport($selector, {formats: ["xlsx", "xls", "csv", "txt"]});
     });
 
